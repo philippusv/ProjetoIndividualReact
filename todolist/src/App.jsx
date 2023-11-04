0e4aeff5574e654a6c2196b1b29f1fc33c9b4c94
philippusv <felipebalbinoribeiro@gmail.com>
2023-11-04 20:47:09 -0300

    atualização

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import Todo from "./components/Todo";
+import TodoForm from "./components/TodoForm";
+import Search from "./components/Search";
+import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -22,22 +26,57 @@ function App() {
     },
   ]);
 
+  const [search, setSearch] = useState("");
+
+  const addTodo = (text, category) => {
+    const newTodos = [
+      ...todos,
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
+
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos];
+    const filteredTodos = newTodos.filter((todo) =>
+      todo.id !== id ? todo : null
+    );
+    setTodos(filteredTodos);
+  };
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos];
+    newTodos.map((todo) =>
+      todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo
+    );
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
-      <div className="todo-list"></div>
-      {todos.map((todo) => (
-        <div className="todo">
-          <div className="content">
-            <p>{todo.text}</p>
-            <p className="category">({todo.category})</p>
-          </div>
-          <div>
-            <button>Completar</button>
-            <button>x</button>
-          </div>
-        </div>
-      ))}
+      <Search search={search} setSearch={setSearch} />
+      <div className="todo-list">
+        {todos
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              completeTodo={completeTodo}
+            />
+          ))}
+      </div>
+      <TodoForm addTodo={addTodo} />
     </div>
   );
 }
