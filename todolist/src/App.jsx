ad1594b917e8fa405d0d35cbd08b5b34f539054f
philippusv <felipebalbinoribeiro@gmail.com>
2023-11-04 10:37:20 -0300

    att

---
+++ todolist/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir pra academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ]);
+
+  return (
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list"></div>
+      {todos.map((todo) => (
+        <div className="todo">
+          <div className="content">
+            <p>{todo.text}</p>
+            <p className="category">({todo.category})</p>
+          </div>
+          <div>
+            <button>Completar</button>
+            <button>x</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
