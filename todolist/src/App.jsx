d0bb1564fccce6b3ed405be68e60efc63b99ac1b
philippusv <felipebalbinoribeiro@gmail.com>
2023-11-04 21:04:40 -0300

    att

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -3,6 +3,7 @@ import Todo from "./components/Todo";
 import TodoForm from "./components/TodoForm";
 import Search from "./components/Search";
 import "./App.css";
+import Filter from "./components/Filter";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -27,6 +28,9 @@ function App() {
   ]);
 
   const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState("");
+  const [sort, setSort] = useState("Asc");
 
   const addTodo = (text, category) => {
     const newTodos = [
@@ -62,8 +66,16 @@ function App() {
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} />
       <div className="todo-list">
         {todos
+          .filter((todo) =>
+            filter === "All"
+              ? true
+              : filter === "Completed"
+              ? todo.isCompleted
+              : !todo.isCompleted
+          )
           .filter((todo) =>
             todo.text.toLowerCase().includes(search.toLowerCase())
           )
--- todolist/src/components/Filter.jsx
+++ todolist/src/components/Filter.jsx
@@ -1,20 +1,15 @@
-const Filter = () => {
+const Filter = ({ filter, setFilter }) => {
   return (
     <div className="filter">
       <h2>Filtrar:</h2>
       <div className="filter-options">
         <div>
           <p>Status:</p>
-          <select>
+          <select value={filter} onChange={(e) => setFilter(e.target.value)}>
             <option value="All">Todas</option>
             <option value="Completed">Completas</option>
             <option value="Incomplete">Incompletas</option>
           </select>
-        </div>
-        <div>
-          <p>Ordem alfabética</p>
-          <button>Asc</button>
-          <button>Desc</button>
         </div>
       </div>
     </div>
